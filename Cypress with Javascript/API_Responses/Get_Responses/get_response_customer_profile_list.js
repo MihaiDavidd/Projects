fc2f0ba9790d5fb347b2f5ed8c9cef7d00c12272
Mihai David <112001027+MihaiDavidd@users.noreply.github.com>
2024-04-10 10:40:31 +0300

    Add files via upload

---
+++ Cypress with Javascript/API_Responses/Get_Responses/get_response_customer_profile_list.js
@@ -0,0 +1,55 @@
+let accessToken
+let shopId
+let credentials
+let api_username
+let api_key_password
+let api_url
+let shop_unique
+let platform 
+
+describe('API Response', function() {
+    before(function() {
+        cy.fixture('credentials').then(function(dataCredentials) {
+            credentials = dataCredentials
+            shopId = Cypress.env("shopId") || "demo01"
+            const shopCredentials = credentials[shopId]
+            api_username = shopCredentials.api_username
+            api_key_password = shopCredentials.api_key_password
+            api_url = shopCredentials.api_url
+            shop_unique = shopCredentials.shop_unique
+            platform = shopCredentials.platform
+        })
+    })
+
+    it('Log In', function() {
+        let requestBody
+        requestBody = {
+            "username": api_username,
+            "key": api_key_password 
+        }
+        cy.request({
+            method: 'POST',
+            url: api_url + "login",
+            body : requestBody
+        }).then(response => {
+            accessToken = response.body.token
+        })
+    })
+
+    it('Append response body to JSON file for customer profile list', function() {
+        cy.request({
+            method: 'GET',
+            url: `${api_url}s/${shop_unique}/customer-profiles?page_size=1`,
+            failOnStatusCode: false,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }).then(response => {
+            const responseBody = response.body
+            const fileName = `response_customer_profile_list_${shop_unique}_${platform}.json`
+            cy.writeFile(`API_Responses/Responses/${fileName}`, responseBody).then(() => {
+                cy.log(`${fileName} updated successfully.`)
+            })
+        })
+    })
+})
---
+++ Cypress with Javascript/API_Responses/Get_Responses/get_response_nps_invitation_serve.js
@@ -0,0 +1,57 @@
+let accessToken
+let shopId
+let credentials
+let api_username
+let api_key_password
+let api_url
+let shop_unique
+let platform 
+let api_serve_id
+
+describe('API Response', function() {
+    before(function() {
+        cy.fixture('credentials').then(function(dataCredentials) {
+            credentials = dataCredentials
+            shopId = Cypress.env("shopId") || "demo01"
+            const shopCredentials = credentials[shopId]
+            api_username = shopCredentials.api_username
+            api_key_password = shopCredentials.api_key_password
+            api_url = shopCredentials.api_url
+            shop_unique = shopCredentials.shop_unique
+            platform = shopCredentials.platform
+            api_serve_id = shopCredentials.api_serve_id
+        })
+    })
+
+    it('Log In', function() {
+        let requestBody
+        requestBody = {
+            "username": api_username,
+            "key": api_key_password 
+        }
+        cy.request({
+            method: 'POST',
+            url: api_url + "login",
+            body : requestBody
+        }).then(response => {
+            accessToken = response.body.token
+        })
+    })
+
+    it('Append response body to JSON file for nps invitation serve', function() {
+        cy.request({
+            method: 'GET',
+            url: `${api_url}nps-invitations/${api_serve_id}/serve`,
+            failOnStatusCode: false,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }).then(response => {
+            const responseBody = response.body
+            const fileName = `response_nps_invitation_serve_${shop_unique}_${platform}.json`
+            cy.writeFile(`API_Responses/Responses/${fileName}`, responseBody).then(() => {
+                cy.log(`${fileName} updated successfully.`)
+            })
+        })
+    })
+})
