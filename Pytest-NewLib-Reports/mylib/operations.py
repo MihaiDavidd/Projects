ec000eb0c3bea19e108d0e798b7a19e09266d14c
Mihai David <112001027+MihaiDavidd@users.noreply.github.com>
2024-09-19 15:52:17 +0300

    Add files via upload

---
+++ Pytest-NewLib-Reports/mylib/operations.py
@@ -0,0 +1,26 @@
+def increment(n):
+    return n + 1
+# ncrement the number by 1.
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
